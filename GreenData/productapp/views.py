67955f5de2946a52d36457c1d4fa38470972d4a2
Renji <dodo.savenay@gmail.com>
2021-04-17 18:05:58 +0300

    nested packaging form working

--- GreenData/productapp/views.py
+++ GreenData/productapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import transaction, IntegrityError
+from django.forms import modelformset_factory
+
+from .models import Product, PackagingInfo
+from .forms import ProductForm, PackagingInfoForm
 
 # Create your views here.
+
+
+def create_product_view(request, *args, **kwargs):
+	ctxt = {}
+
+	PackagingFormSet = modelformset_factory(model=PackagingInfo, form=PackagingInfoForm )
+
+	product_form = ProductForm(request.POST or None)
+
+	packaging_formset = PackagingFormSet(
+		request.POST or None,
+		queryset=PackagingInfo.objects.none(),
+		prefix='packaging_info'
+		)
+	
+	if request.method == 'POST':
+		if product_form.is_valid() and packaging_formset.is_valid():
+			try:
+				with transaction.atomic():
+					product = product_form.save(commit=False)
+					product.save
+					for pkg in packaging_formset:
+						packaging = pkg.save(commit=False)
+						c.product = product
+						c.save()
+			except IntegrityError:
+				raise "Form ERROR"
+			product_form = ProductForm()
+			packaging_formset = PackagingFormSet(queryset=PackagingInfo.objects.none(), prefix='packaging_info')
+			return redirect('/')
+	
+	ctxt['product_form'] = product_form
+	ctxt['packaging_formset'] = packaging_formset
+	return render(request, 'create_product.html', ctxt)
