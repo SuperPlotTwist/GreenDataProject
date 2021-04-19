7946bcebf81825769ff8294847ef37a9388ce9c8
Clarisse Blanco <clarisse.blanco@epita.fr>
2021-04-19 22:41:36 +0300

    added a first password change page but still needs to be improved

--- GreenData/members/urls.py
+++ GreenData/members/urls.py
@@ -4,10 +4,12 @@ from .views import UserRegisterView, UserEditView
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/', auth_view.PasswordChangeView.as_view(template_name='registration/change_password.html'))
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
 ]
