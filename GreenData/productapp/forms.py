84b5a9d34403f0ced2b9c52315f8e61f69886cb1
Renji <dodo.savenay@gmail.com>
2021-04-20 15:55:17 +0300

    fixed disappearing barcode field on product creation form

--- GreenData/productapp/forms.py
+++ GreenData/productapp/forms.py
@@ -2,49 +2,38 @@ from .models import Product, PackagingInfo
 from django import forms
 from .presets import CATEGORIES, UNITS, MATERIALS
 
+
 class ProductForm(forms.ModelForm):
 
-	category = forms.TypedChoiceField(choices=CATEGORIES)
-	quantity_unit = forms.TypedChoiceField(choices=UNITS)
+    category = forms.TypedChoiceField(choices=CATEGORIES)
+    quantity_unit = forms.TypedChoiceField(choices=UNITS)
 
-	class Meta:
-		model = Product
+    class Meta:
+        model = Product
 
-		fields = [
-			'name',
-			'brand',
-			'barcode',
-			'category',
-			'origin',
-			'quantity',
-			'quantity_unit'
-		]
-	
-	def __init__(self, *args, edit_mode=False, **kwargs):
-		if edit_mode and self.base_fields.get('barcode'):
-			self.base_fields.pop('barcode')
-		super(ProductForm, self).__init__(*args, **kwargs)
+        fields = [
+            'name', 'brand', 'barcode', 'category', 'origin', 'quantity',
+            'quantity_unit'
+        ]
+
+    def __init__(self, *args, edit_mode=False, **kwargs):
+        self.base_fields['barcode'].disabled = edit_mode
+        super(ProductForm, self).__init__(*args, **kwargs)
 
 
 class PackagingInfoForm(forms.ModelForm):
-	
-	material = forms.ChoiceField(choices=MATERIALS, widget=forms.Select())
 
-	class Meta:
-		model = PackagingInfo
+    material = forms.ChoiceField(choices=MATERIALS, widget=forms.Select())
 
-		fields = [
-			'element',
-			'material',
-			'mass',
-			'is_recyclable',
-			'is_recycled'
-		]
-		exclude = [
-			'id'
-		]
+    class Meta:
+        model = PackagingInfo
 
-	def __init__(self, *args, **kwargs):
-		super(PackagingInfoForm, self).__init__(*args, **kwargs)
-		for v_field in self.visible_fields():
-			v_field.field.widget.attrs['class'] = 'formset-field'
+        fields = [
+            'element', 'material', 'mass', 'is_recyclable', 'is_recycled'
+        ]
+        exclude = ['id']
+
+    def __init__(self, *args, **kwargs):
+        super(PackagingInfoForm, self).__init__(*args, **kwargs)
+        for v_field in self.visible_fields():
+            v_field.field.widget.attrs['class'] = 'formset-field'
