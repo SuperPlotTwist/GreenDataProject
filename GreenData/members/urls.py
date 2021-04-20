d9ab77495473bf6b6a5ace056c6ffd965384e9f5
Clarisse Blanco <clarisse.blanco@epita.fr>
2021-04-20 10:21:08 +0300

    added password change page and styled it with bootstrap

--- GreenData/members/urls.py
+++ GreenData/members/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    path('password/', auth_view.PasswordChangeView.as_view(template_name='registration/change_password.html'))
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
+    path('password_success/', views.PasswordSuccess, name = 'password_success'),
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
 ]
