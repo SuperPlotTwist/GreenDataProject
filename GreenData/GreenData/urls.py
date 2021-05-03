d6a1cb4ce3e5e6c3ef9296b1d2973af189b69010
Renji <dodo.savenay@gmail.com>
2021-05-03 14:23:54 +0300

    deleted old products html templates

--- GreenData/GreenData/urls.py
+++ GreenData/GreenData/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
-	path('fixed_product/', include('productapp.urls')),
+	path('', include(('productapp.urls', 'productapp'), namespace="productapp")),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 ]
--- GreenData/productapp/urls.py
+++ GreenData/productapp/urls.py
@@ -4,7 +4,7 @@ from .views import create_product_view, product_detail_view, edit_product_view
 
 
 urlpatterns = [
-	path('create', create_product_view, name='create_product'),
+	path('add_product', create_product_view, name='add_product'),
 	path('product/<str:pk>/', product_detail_view, name='product_detail'),
 	path('product/<str:pk>/edit', edit_product_view, name='edit_product'), 
 ]
